8894cacb13d4bcdf2fd2bb3ae6acbcd906047cae
Tom Sullivan <tom@msbit.com.au>
2021-05-11 16:29:14 +1000

    Set `MainActivity` view to `TextView`

--- app/src/main/java/au/com/msbit/gradle_android_minimal/MainActivity.java
+++ app/src/main/java/au/com/msbit/gradle_android_minimal/MainActivity.java
@@ -1,5 +1,30 @@
 package au.com.msbit.gradle_android_minimal;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {}
+import static android.view.Gravity.CENTER;
+import static android.widget.TextView.AUTO_SIZE_TEXT_TYPE_UNIFORM;
+
+public class MainActivity extends Activity {
+  int _count = 0;
+
+  @Override
+  protected void onCreate(Bundle saved) {
+    super.onCreate(saved);
+
+    var view = new TextView(this);
+
+    view.setAutoSizeTextTypeWithDefaults(AUTO_SIZE_TEXT_TYPE_UNIFORM);
+    view.setGravity(CENTER);
+    view.setOnClickListener(v -> ((TextView)v).setText(count()));
+    view.setText(count());
+
+    setContentView(view);
+  }
+
+  String count() {
+    return String.valueOf(_count++);
+  }
+}
